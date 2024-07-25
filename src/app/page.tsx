87bd1ddae6190976772d41dcdfc1640b455a7b5e
Alex <86182397+alehxs@users.noreply.github.com>
2024-07-24 22:57:25 -0500

    small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
+      <h1 className={styles.title}>Welcome to F1 Metrics!</h1>
     </main>
   );
 }
